e961fb533aee58cd52ec30bada48be35c5908dfa
bjlamarca <bryanlamarca@outlook.com>
2025-02-03 20:43:38 -0500

    Git Issues with Control

--- showdemon/ui/control.py
+++ showdemon/ui/control.py
@@ -3,11 +3,12 @@ from PySide6.QtWidgets import (QWidget, QPushButton, QMessageBox, QVBoxLayout, Q
 
 from PySide6.QtCore import Qt
 from devices.interfaces import DMXInterface
-from devices.models import Device, Channel, ChannelParameter, DeviceFeature
+from devices.models import Device, Channel, ChannelParameter, DeviceFeature, LibraryChannel
 
 from devices.signals import dmx_signal
 import uuid
 from django.dispatch import Signal, receiver
+from static.style import resources_rc
 
 @receiver(dmx_signal)
 def handle_device_signal(sender, **kwargs):
@@ -30,6 +31,7 @@ class MainControlWindow(QMainWindow):
         lbl_dev_choice = QLabel("Device")
         lbl_dev_choice.setMinimumWidth(100)
         self.device_list = QComboBox()
+        #self.device_list.setStyleSheet("QComboBox::down-arrow {image: url('C:/Dev/showdemon/showdemon/static/fugue-2x-icons/icons-2x/arrow-down.png');}")
         #self.device_list.currentIndexChanged.connect(self.device_selected)
         device_qs = Device.objects.all()
         self.device_list.addItem("Select Device", 0)
@@ -74,6 +76,7 @@ class DMXChannelWindow(QMainWindow):
     def __init__(self, parent=None, device_id=None):
         super().__init__(parent)
         self.device_id = device_id
+        self.channel_list = []
 
         dmx_signal.connect(self.receive_dmx_signal)
         self.setWindowTitle("Control")
@@ -119,6 +122,7 @@ class DMXChannelWindow(QMainWindow):
         channel_qs = Channel.objects.filter(device=self.device).order_by('channel_number')
         if channel_qs:
             for channel in channel_qs:
+                    self.channel_list.append(channel.system_channel)
                     slide_layout = QVBoxLayout()
                     slide_lbl = QLabel(channel.library_channel.name + ' ' + str(channel.system_channel))
                     slide_slider = QSlider()
@@ -139,12 +143,12 @@ class DMXChannelWindow(QMainWindow):
     def receive_dmx_signal(self, sender, **kwargs):
         if sender == 'DMX-INTERFACE':
             data = kwargs.get('data_dict')
-            #print('Sender', sender, 'Data', data)
             if data['requester'] != self.uuid:
-                channel = data['channel']
-                value = data['value']
-                slider = self.slide_ctl_frame.findChild(QSlider, f'slide_{channel}')
-                slider.setValue(value)
+                if data['channel'] in self.channel_list:
+                    channel = data['channel']
+                    value = data['value']
+                    slider = self.slide_ctl_frame.findChild(QSlider, f'slide_{channel}')
+                    slider.setValue(value)
                       
         
 
@@ -157,50 +161,193 @@ class DMXFeatureWindow(QMainWindow):
         super().__init__(parent)
         self.device_id = device_id
         self.device = Device.objects.get(pk=device_id)
+       
         
         self.setWindowTitle("Feature Control")
         main_widget = QWidget()
         self.setCentralWidget(main_widget)
         self.layout = QVBoxLayout(main_widget)
         
+        horz_layout = QHBoxLayout()
+
         self.frame1 = QFrame()
         self.frame1.setMinimumHeight(300)
 
         layout1_layout = QVBoxLayout()
         layout1_lbl = QLabel('Features')
         layout1_layout.addWidget(layout1_lbl)
-        layout1_layout.addWidget(SelectorWidget(device_id=self.device_id, feature_id=0))
+        layout1_layout.addWidget(DMXSelectorWidget(device_id=self.device_id, feature_id=6))
         self.frame1.setLayout(layout1_layout)
-        self.layout.addWidget(self.frame1)
+        horz_layout.addWidget(self.frame1)
+        
+        self.frame2 = QFrame()
+        self.frame2.setMinimumHeight(300)
+
+        layout2_layout = QVBoxLayout()
+        layout2_lbl = QLabel('Features')
+        layout2_layout.addWidget(layout2_lbl)
+        layout2_layout.addWidget(DMXDimmerWidget(device_id=self.device_id, feature_id=4))
+        self.frame2.setLayout(layout2_layout)
+        horz_layout.addWidget(self.frame2)
+        
+
+
+        self.layout.addLayout(horz_layout)
 
         
                  
 
-class SelectorWidget(QWidget):
+class DMXSelectorWidget(QWidget):
     def __init__(self, device_id, feature_id):
         super().__init__()
         self.device_id = device_id
         self.feature_id = feature_id
-        #device_qs = Device.objects.get(pk=device_id)
-        #feature_qs = DeviceFeature.objects.get(pk=feature_id)
+        device_qs = Device.objects.get(pk=device_id)
+        feature_qs = DeviceFeature.objects.get(pk=feature_id)
+        lib_channel_qs = LibraryChannel.objects.filter(device_feature=feature_qs)
+        
+        lib_channel = lib_channel_qs[0]
+        self.parm_qs = ChannelParameter.objects.filter(library_channel=lib_channel).order_by('int_min')
+        channel_qs = Channel.objects.filter(library_channel=lib_channel, device=device_qs)
+        self.channel = channel_qs[0]
+        self.current_value = self.channel.int_value
+        
 
-
+                
+        dmx_signal.connect(self.receive_dmx_signal)
+        self.dmx = DMXInterface()
+        self.uuid = uuid.uuid4()
         self.layout = QVBoxLayout()
-        
-        layout_main = QHBoxLayout()
-        main_lbl = QLabel('Main')
-        layout_main.addWidget(main_lbl)
-        layout_main.addStretch()
+        horz_layout = QHBoxLayout() 
+        title_layout = QHBoxLayout()
+        title_feat_txt_lbl = QLabel(feature_qs.name)
+        title_feat_txt_lbl.setStyleSheet('font-weight: bold;')
+        title_layout.addWidget(title_feat_txt_lbl)
+        title_layout.addStretch()
+        self.layout.addLayout(title_layout)
 
-        self.layout.addLayout(layout_main)
+
+        ctrl_layout = QVBoxLayout()
+        self.feat_combo = QComboBox()
+        for parm in self.parm_qs:
+            self.feat_combo.addItem(parm.name, parm.pk)
+        self.feat_combo.currentIndexChanged.connect(self.combo_changed)
+        ctrl_layout.addWidget(self.feat_combo)
+        self.feat_slider = QSlider()
+        self.feat_slider.setFixedHeight(250)
+        self.feat_slider.setDisabled(True)
+        ctrl_layout.addWidget(self.feat_slider)
+        ctrl_layout.addStretch()
+
         
+        horz_layout.addLayout(ctrl_layout)
         
-        
+        horz_layout.addStretch()
+        self.layout.addLayout(horz_layout)
+        self.layout.addStretch()
         self.setLayout(self.layout)
+
+        self.set_current_value(self.current_value)
+
+    def combo_changed(self):
+        parm_qs = ChannelParameter.objects.get(pk=self.feat_combo.currentData())
+        if parm_qs.allow_fading:
+            self.feat_slider.setDisabled(False)
+            self.feat_slider.setRange(parm_qs.int_min, parm_qs.int_max)
+            self.feat_slider.setValue(parm_qs.int_min)
+        else:
+            self.feat_slider.setValue(0)
+            self.feat_slider.setDisabled
+
+    def set_slider(self):
+        pass
+
+    def set_current_value(self, value):
+        index = 0
+        for parm in self.parm_qs:
+            if value >= parm.int_min and value <= parm.int_max:
+                self.feat_combo.setCurrentIndex(index)
+                if parm.allow_fading:
+                    self.feat_slider.setDisabled(False)
+                    self.feat_slider.setRange(parm.int_min, parm.int_max)
+                    self.feat_slider.setValue(value)
+                else:
+                    self.feat_slider.setValue(0)
+                    self.feat_slider.setDisabled(True)
+                break
+            index += 1
+
+    def receive_dmx_signal(self, sender, **kwargs):
+        if sender == 'DMX-INTERFACE':
+            data = kwargs.get('data_dict')
+            if data['requester'] != self.uuid:
+                if data['channel'] == self.channel.system_channel:
+                    channel = data['channel']
+                    value = data['value']
+                    self.set_current_value(value)
+                    self.current_value = value
         
         
+class DMXDimmerWidget(QWidget):
+    def __init__(self, device_id, feature_id):
+        super().__init__()
+        self.device_id = device_id
+        self.feature_id = feature_id
+        device_qs = Device.objects.get(pk=device_id)
+        feature_qs = DeviceFeature.objects.get(pk=feature_id)
+        lib_channel_qs = LibraryChannel.objects.filter(device_feature=feature_qs)
+        lib_channel = lib_channel_qs[0]
+        channel_qs = Channel.objects.filter(library_channel=lib_channel, device=device_qs)
+        self.channel = channel_qs[0]
+        self.current_value = self.channel.int_value
+
         
-        self.device_list = QComboBox()
+
+
+        dmx_signal.connect(self.receive_dmx_signal)
+        self.dmx = DMXInterface()
+        self.uuid = uuid.uuid4()
+        self.layout = QVBoxLayout()
+        horz_layout = QHBoxLayout() 
+        title_layout = QHBoxLayout()
+        title_feat_txt_lbl = QLabel(feature_qs.name)
+        title_feat_txt_lbl.setStyleSheet('font-weight: bold;')
+        title_layout.addWidget(title_feat_txt_lbl)
+        title_layout.addStretch()
+        self.layout.addLayout(title_layout)
+
+        ctrl_layout = QVBoxLayout()
+        self.feat_slider = QSlider()
+        self.feat_slider.setFixedHeight(250)
+        self.feat_slider.setDisabled(True)
+        ctrl_layout.addWidget(self.feat_slider)
+        ctrl_layout.addStretch()
+
+
+        horz_layout.addLayout(ctrl_layout)
+        
+        horz_layout.addStretch()
+        self.layout.addLayout(horz_layout)
+        self.layout.addStretch()
+        self.setLayout(self.layout)
+
+        self.set_current_value(self.current_value)
+
+
+    def set_current_value(self, value):
+        self.feat_slider.setValue(value)
+
+    def receive_dmx_signal(self, sender, **kwargs):
+        if sender == 'DMX-INTERFACE':
+            data = kwargs.get('data_dict')
+            if data['requester'] != self.uuid:
+                if data['channel'] == self.channel.system_channel:
+                    channel = data['channel']
+                    value = data['value']
+                    self.set_current_value(value)
+                    self.current_value = value
+
+
         
 
 
